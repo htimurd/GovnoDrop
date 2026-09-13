dadd997ac11f4d2f2c35604d224a45ea75babe97
htimurd <cu16106timur@gmail.com>
2026-09-13 16:24:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 _raw_admins = os.environ.get("ADMIN_IDS", "8080874290")
 ADMIN_IDS = {int(x) for x in _raw_admins.split(",") if x.strip().isdigit()}
 
-WEBHOOK_HOST = os.environ.get("WEBHOOK_HOST", "")  # например https://govno-drop.onrender.com
+WEBHOOK_HOST = os.environ.get("WEBHOOK_HOST", "")  # например https://govno-drop-bot.onrender.com
 WEBHOOK_PATH = "/webhook"
 WEBHOOK_SECRET = os.environ.get("WEBHOOK_SECRET", "govnodrop-secret")
 
